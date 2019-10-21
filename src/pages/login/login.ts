78fe8192af66ee5d1e242b54c04981e982883f94
Mohamed Ramadan <mohamed_ramadan154@hotmail.com>
2019-10-21 22:44:32 +0200

    home

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {SignupPage} from "../signup/signup";
+import {HomePage} from "../home/home";
 
 @Component({
   selector: 'page-login',
@@ -14,6 +15,10 @@ export class LoginPage {
 
   openSignup() {
     this.navCtrl.push(SignupPage);
+  }
+
+  onGoHome() {
+    this.navCtrl.setRoot(HomePage);
   }
 
 }
